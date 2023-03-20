efb6ea56d08888541c56976578362e6c0e53e3db
connor <connor@dunamu.com>
2023-03-20 23:32:26 +0900

    chore: 느낌표 추가

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   { owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World !}
-    const params = context.issue({ body: "Hello Kakao!" });
+    const params = context.issue({ body: "Hello Kakao!!!" });
 
     // Post a comment on the issue
     return context.octokit.issues.createComment(params);
