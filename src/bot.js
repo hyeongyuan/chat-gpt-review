db93ac721e3f78cd5c058d061cf4ac44a0c02e79
connor <connor@dunamu.com>
2023-03-21 23:41:55 +0900

    feat: 코멘트를 가져와 내용을 다는 기능 추가

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,13 @@
+const axios = require("axios");
+
 module.exports = (app) => {
   app.on("issues.opened", async (context) => {
-    const params = context.issue({ body: "Hello World" });
+    const owner = 'test';
+    const repo = 'issue-bot';
 
-    console.log(params);
+    const { data } = await axios.get(`https://api.github.com/repos/${owner}/${repo}/issues/comments/10`);
+
+    const params = context.issue({ body: data.body });
 
     return context.octokit.issues.createComment(params);
   });
